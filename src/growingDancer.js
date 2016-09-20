ea356604f9dab3b43dad10c1894de8d844417c78
kkinaman <kkinaman419@gmail.com>
2016-09-20 12:27:42 -0700

    Fix bug (for real) for growingDancers
    - Corrected comparison of span height to determine growing/shrinking

--- src/growingDancer.js
+++ src/growingDancer.js
@@ -20,26 +20,25 @@ GrowingDancer.prototype.step = function() {
   // call the old version of step at the beginning of any call to this new version of step
   this.oldStep();
   var dancer = this;
+  console.log(dancer.$node.css('height'));
   // console.log(dancer.$node.css('height'));
-  if (dancer.$node.css('height') >= '300px') {
+  if (parseInt(dancer.$node.css('height')) >= 150) {
     dancer.growing = false;
+  } else if (parseInt(dancer.$node.css('height')) <= 50) {
+    dancer.growing = true;
   }
   if (dancer.growing) {
     dancer.$node.animate({
       height: '+=30px',
       width: '+=30px'
-    }, this.timeBetweenSteps, function() {
-    });
-    
-    
+    }, this.timeBetweenSteps / 2, function() {
+    }); 
   } else {
     dancer.$node.animate({
       height: '-=30px',
       width: '-=30px'
-    }, this.timeBetweenSteps, function() {
-      if (dancer.$node.css('height') <= '50px') {
-        dancer.growing = true;
-      }
+    }, this.timeBetweenSteps / 2, function() {
+      
     });
     
   }
