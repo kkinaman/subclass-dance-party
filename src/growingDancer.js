968ea6d92b5d1784a7c8edf70905e03be695b79b
kkinaman <kkinaman419@gmail.com>
2016-09-20 11:15:33 -0700

    Fix bug with evergrowing growing dancer
    - Growing dancer was growing too big because of scheduling conflict (step versus animation duration)

--- src/growingDancer.js
+++ src/growingDancer.js
@@ -2,6 +2,7 @@ var GrowingDancer = function(top, left, timeBetweenSteps) {
   // debugger;
   Dancer.call(this, top, left, timeBetweenSteps);
   this.growing = true;
+  this.timeBetweenSteps = 500;
 
   // we plan to overwrite the step function below, but we still want the superclass step behavior to work,
   // so we must keep a copy of the old version of this function
@@ -20,14 +21,14 @@ GrowingDancer.prototype.step = function() {
   this.oldStep();
   var dancer = this;
   // console.log(dancer.$node.css('height'));
-  if (dancer.$node.css('height') >= '500px') {
+  if (dancer.$node.css('height') >= '300px') {
     dancer.growing = false;
   }
   if (dancer.growing) {
     dancer.$node.animate({
       height: '+=30px',
       width: '+=30px'
-    }, 100, function() {
+    }, this.timeBetweenSteps, function() {
     });
     
     
@@ -35,7 +36,7 @@ GrowingDancer.prototype.step = function() {
     dancer.$node.animate({
       height: '-=30px',
       width: '-=30px'
-    }, 100, function() {
+    }, this.timeBetweenSteps, function() {
       if (dancer.$node.css('height') <= '50px') {
         dancer.growing = true;
       }
